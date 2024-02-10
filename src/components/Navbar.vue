e9f08cbaea2fdf94f6bc654bfca93d7f1a403602
zyneman <pushpendersolanki_co21a6_23@dtu.ac.in>
2024-02-10 06:56:01 +0530

    Refactor Navbar component styling for responsiveness

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,48 @@
 <template>
-    <nav class="navbar">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/star">Star</router-link>
-      <router-link to="/game">Game</router-link>
-    </nav>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Navbar'
-  }
-  </script>
-  
-  <style scoped>
+  <nav class="navbar">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/star">Star</router-link>
+    <router-link to="/game">Game</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  border-bottom: 1px solid var(--primary-color);
+  padding-bottom: 10px;
+}
+
+.navbar a {
+  margin-left: 10px;
+  color: var(--text-color);
+  text-decoration: none;
+  font-size: 1.2em; /* Increase the font size */
+}
+
+.navbar a:hover {
+  color: var(--accent-color);
+}
+
+/* Responsive styles */
+@media screen and (max-width: 600px) {
   .navbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-    border-bottom: 1px solid var(--primary-color);
-    padding-bottom: 10px;
-    font-size: 1.2rem;
+    flex-direction: column;
+    align-items: flex-start;
   }
-  
+
   .navbar a {
-    margin-left: 10px;
-    color: var(--text-color);
-    text-decoration: none;
+    margin-bottom: 5px;
   }
-  
-  .navbar a:hover {
-    color: var(--accent-color);
-  }
-  </style>
+}
+</style>
